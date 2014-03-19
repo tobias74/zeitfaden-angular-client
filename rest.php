16496a38ea236ef8b8ab5a58db1191adec44763c
root <root@s17332969.onlinehome-server.info>
2014-03-19 21:14:11 +0100

    some

--- rest.php
+++ rest.php
@@ -1,4 +1,12 @@
 <?php 
+
+ini_set("session.save_handler","redis");
+session_save_path("tcp://213.165.93.33:6379");
+ini_set("session.cookie_domain",".zeitfaden.com");
+
+ini_set("max_execution_time",43200);
+
+
 require_once('../application/application-files.php');
 $applicationIni = parse_ini_file('../application/configuration/application.ini',true);
 
